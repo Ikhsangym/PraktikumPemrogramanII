90e17dc84d97d51061f0d56b7ff6225585eed558
ikhsangym <114869784+Ikhsangym@users.noreply.github.com>
2023-10-03 15:35:49 +0700

    praktikum pemrograman 2
    bang tutor bang

---
+++ PraktikumPemrograman2/src/Praktikum0/PRAK001_2210817310005_IG.java
@@ -0,0 +1,7 @@
+public class PRAK001_2210817310005_IG {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi, Ikhsan Gymnastiar");
+        System.out.println("Selamat Siang, Ikhsan Gymnastiar");
+        System.out.println("Selamat Malam, Ikhsan Gymnastiar");
+    }
+}
---
+++ PraktikumPemrograman2/src/Praktikum0/PRAK004_2210817310005_IG.java
@@ -0,0 +1,7 @@
+public class PRAK004_2210817310005_IG {
+    public static void main(String[] args) {
+        String nama ="Ikhsan Gymnastiar";
+        int umur =19;
+        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur saya " + umur );
+    }
+}
