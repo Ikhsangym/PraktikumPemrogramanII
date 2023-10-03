f354f0da321805c124790caab0300b0a32289dae
ikhsangym <114869784+Ikhsangym@users.noreply.github.com>
2023-10-03 21:00:49 +0700

    Delete PraktikumPemrograman2 directory

--- PraktikumPemrograman2/src/Praktikum0/PRAK004_2210817310005_IG.java
+++
@@ -1,7 +0,0 @@
-public class PRAK004_2210817310005_IG {
-    public static void main(String[] args) {
-        String nama ="Ikhsan Gymnastiar";
-        int umur =19;
-        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur saya " + umur );
-    }
-}
