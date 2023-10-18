02c9d3b301743e5ac97561b0a8b506de170de292
ikhsangym <114869784+Ikhsangym@users.noreply.github.com>
2023-10-18 09:32:49 +0700

    Add files via upload

---
+++ PraktikumPemrograman2/src/Praktikum2/PRAK201_2210817310005_IKHSANGYMNASTIAR/Soal1Main.java
@@ -0,0 +1,11 @@
+package Praktikum2.PRAK201_2210817310005_IKHSANGYMNASTIAR;
+public class Soal1Main {
+    public static void main(String[] args) {
+        Buah apel = new Buah("apel", 0.4,40,7000 );
+        Buah mangga = new Buah("mangga", 0.2,15, 3500 );
+        Buah alpukat = new Buah("alpukat", 0.25,12,10000 );
+        apel.Infobuah();
+        mangga.Infobuah();
+        alpukat.Infobuah();
+    }
+}
